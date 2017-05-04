7eee1c21aa58dfc14eff10fbe36ab949d6db4cab
Kavya Sukumar <kavya.sukumar@outlook.com>
2017-05-04 13:09:49 -0700

    provider mgmnt js

---
+++ source/javascripts/group_management.js
@@ -0,0 +1,48 @@
+//= require _kinto_init
+
+var kintoBucket = kintoBucket || window.getKintoBucket('http://kinto.voxmedia.com/v1','vox-aca-dashboard');
+
+/* If you use any Foundation javascript add the following line to the top of
+ * of this file...
+ *   //= require foundation
+ * then uncomment the following line.
+ */
+// $(document).foundation();
+
+/* If you would like to use Javascript templates, you can enable them by adding
+ * the following line to the top of this file
+ *   //=require_tree ./_templates
+ * you can then use the templates like this:
+ *   var template = window.JST['_templates/example'];
+ *   var html = template(***REMOVED***my_var: 'Hello!', list: [1, 2, 3, 4]***REMOVED***);
+ *   $('#example').html(html);
+ * Template files should have the extension `.jst.ejs` to be properly loaded.
+ */
+
+(function() ***REMOVED***
+  // Application code goes here
+
+  $(document).ready(function() ***REMOVED***
+    // Initialize lazy load
+    $('.lazy').lazyload(***REMOVED***
+      threshold : 0,
+      failure_limit: 999,
+      effect: 'fadeIn',
+      data_attribute_queries: [
+        ***REMOVED***media: "(max-width: 1600px)", data_name: "x-large"***REMOVED***,
+        ***REMOVED***media: "(max-width: 1200px)", data_name: "large"***REMOVED***,
+        ***REMOVED***media: "(max-width: 900px)", data_name: "medium"***REMOVED***,
+        ***REMOVED***media: "(max-width: 640px)", data_name: "small"***REMOVED***,
+        ***REMOVED***media: "(max-width: 400px)", data_name: "x-small"***REMOVED***
+      ]
+    ***REMOVED***);
+
+    // Kinto client documentation at https://doc.esdoc.org/github.com/Kinto/kinto-client/#collections
+    // You can now access collections and records in Kinto like this
+    // var myCollection = kintoBucket.collection('YOUR_COLLECTION_NAME');
+    // myCollection.createRecord(***REMOVED***test:'value'***REMOVED***);
+    // myCollection.listRecords().then(function(data) ***REMOVED***console.log(data);***REMOVED***);
+
+    // Initialize your code here
+  ***REMOVED***);
+***REMOVED***)();
