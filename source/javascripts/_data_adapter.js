2cbde26a4705e48b01921e8a9cd14ae401b27094
Kavya Sukumar <kavya.sukumar@outlook.com>
2017-05-09 09:55:24 +0530

    add adapter model for data fetch

--- source/javascripts/_data_adapter.js
+++ source/javascripts/_data_adapter.js
@@ -5,62 +5,97 @@
 /* globals KINTO_TOKEN, console */
 
 
-var DataAdapter = (function()***REMOVED***
+var DataAdapter = (function() ***REMOVED***
+  var instance;
 
-  // Private properties and methods
-  var _kintoBucket, _coverage17Collection;
+  window.localStorage.setItem('kintoToken', KINTO_TOKEN);
+  var _kintoBucket = _kintoBucket || window.getKintoBucket('https://voxmedia-kinto.herokuapp.com/v1', 'vox-aca-dashboard', true);
 
-  var _data = ***REMOVED***
-    coverage_2017: ***REMOVED******REMOVED***,
-    provider_groups: ***REMOVED******REMOVED***
-  ***REMOVED***;
-  var init = function()***REMOVED***
-    window.localStorage.setItem('kintoToken', KINTO_TOKEN);
-     _kintoBucket  = _kintoBucket || window.getKintoBucket('https://voxmedia-kinto.herokuapp.com/v1','vox-aca-dashboard', true);
-     _coverage17Collection = _kintoBucket.collection('coverage-2017');
-     // myCollection.createRecord(***REMOVED***test:'value'***REMOVED***);
-     // myCollection.listRecords().then(function(data) ***REMOVED***console.log(data);***REMOVED***);
-  ***REMOVED***;
+  var init = function() ***REMOVED***
 
-  var _getCoverageData = function(refresh)***REMOVED***
-    if(!refresh && !_.isEmpty(_data.coverage_2017))***REMOVED***
-      console.log('data exists');
-      console.log(_data.coverage_2017);
-      return _data.coverage_2017;
-    ***REMOVED***
+    // Private properties and methods
+    var _data = ***REMOVED******REMOVED***,
+      _staleBit = ***REMOVED******REMOVED***;
+    var _instance = ***REMOVED******REMOVED***;
 
-    _coverage17Collection.listRecords(***REMOVED***
-      limit: 1000, pages: Infinity
-      ***REMOVED***).then(function(response)***REMOVED***
-          _data.coverage_2017 = response.data;
-      ***REMOVED***).catch(function(error)***REMOVED***
+    var _getCoverageData = function(year, refresh, callBackFunction) ***REMOVED***
+      var dataKey = 'coverage-' + year;
+      if (!refresh && _data[dataKey]) ***REMOVED***
+        console.log('data exists');
+        callBackFunction.call(this, _data[dataKey]);
+      ***REMOVED***
+
+      var collection = _kintoBucket.collection(dataKey);
+      _staleBit[dataKey] = false;
+
+      collection.listRecords(***REMOVED***
+        limit: 1000,
+        pages: Infinity
+      ***REMOVED***).then(function(response) ***REMOVED***
+        _data[dataKey] = response.data;
+        callBackFunction.call(this, response.data);
+      ***REMOVED***).catch(function(error) ***REMOVED***
+        _staleBit[dataKey] = false;
         console.log(error);
       ***REMOVED***);
-  ***REMOVED***;
+    ***REMOVED***;
 
-  //Public properties and methods
-  var adapter = ***REMOVED******REMOVED***;
+    var _updateCoverageData = function(year, records, callBackFunction) ***REMOVED***
+      var dataKey = 'coverage-' + year,
+        collection = _kintoBucket.collection(dataKey),
+        recordCount = records.count,
+        processedCount = 0;
 
-  adapter.getCoverage17 = function(refresh)***REMOVED***
-    _getCoverageData(refresh);
-  ***REMOVED***;
+      _staleBit[dataKey] = true;
 
-  adapter.updateCoverage17 = function(records)***REMOVED***
-    records.slice(100).each(function(subset)***REMOVED***
-      _coverage17Collection.batch(function(batch)***REMOVED***
-        for (var i = 0; i <= subset.length; i++)***REMOVED***
-          batch.updateRecord(subset[i]);
-        ***REMOVED***
-      ***REMOVED***).then(function(response)***REMOVED***
-        console.log(response);
+      records.slice(100).each(function(subset) ***REMOVED***
+        collection.batch(function(batch) ***REMOVED***
+          for (var i = 0; i <= subset.length; i++) ***REMOVED***
+            batch.updateRecord(subset[i]);
+          ***REMOVED***
+        ***REMOVED***).then(function(response) ***REMOVED***
+          processedCount += 100;
+          if (processedCount >= recordCount) ***REMOVED***
+            callBackFunction.call(this);
+          ***REMOVED***
+        ***REMOVED***).catch(function(error) ***REMOVED***
+          console.log(error);
+        ***REMOVED***);
       ***REMOVED***);
-    ***REMOVED***);
+    ***REMOVED***;
+
+    //Public properties and methods
+    _instance.getCoverage17 = function(refresh, callBackFunction) ***REMOVED***
+      _getCoverageData(2017, refresh, callBackFunction);
+    ***REMOVED***;
+
+    _instance.updateCoverage17 = function(records, callBackFunction) ***REMOVED***
+      _updateCoverageData(2017, refresh, callBackFunction);
+    ***REMOVED***;
+
+    _instance.getAllData = function(refresh) ***REMOVED***
+      _getCoverageData('2014', refresh);
+      _getCoverageData('2015', refresh);
+      _getCoverageData('2016', refresh);
+      _getCoverageData('2017', refresh);
+      return _data;
+    ***REMOVED***;
+
+    return _instance;
+  ***REMOVED***
+
+  return ***REMOVED***
+
+    // Get the Singleton instance if one exists
+    // or create one if it doesn't
+    getInstance: function() ***REMOVED***
+
+      if (!instance) ***REMOVED***
+        instance = init();
+      ***REMOVED***
+
+      return instance;
+    ***REMOVED***
+
   ***REMOVED***;
-
-
-  // self init
-  init();
-
-  return adapter;
-
 ***REMOVED***)();
