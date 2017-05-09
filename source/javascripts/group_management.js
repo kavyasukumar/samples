b442a12650ccaddde284a475b0dd2d20dd919433
Kavya Sukumar <kavya.sukumar@outlook.com>
2017-05-09 09:55:48 +0530

    add js for group mgmnt

--- source/javascripts/group_management.js
+++ source/javascripts/group_management.js
@@ -1,5 +1,3 @@
-
-
 /* If you would like to use Javascript templates, you can enable them by adding
  * the following line to the top of this file
  *   //=require_tree ./_templates
@@ -11,36 +9,12 @@
  */
 
 (function() ***REMOVED***
-  // Application code goes here
-  // console.log('Sponge Bob Squarepants');
-  //
-  //
-  // var somefunc = function () ***REMOVED***
-  //   console.log('No. This is patrick');
-  // ***REMOVED***;
+  var test = function (response) ***REMOVED***
+    console.log(response.length);
+  ***REMOVED***;
 
-  // $(document).ready(function() ***REMOVED***
-  //   console.log('hello there');
-  //   // Initialize lazy load
-  //   $('.lazy').lazyload(***REMOVED***
-  //     threshold : 0,
-  //     failure_limit: 999,
-  //     effect: 'fadeIn',
-  //     data_attribute_queries: [
-  //       ***REMOVED***media: "(max-width: 1600px)", data_name: "x-large"***REMOVED***,
-  //       ***REMOVED***media: "(max-width: 1200px)", data_name: "large"***REMOVED***,
-  //       ***REMOVED***media: "(max-width: 900px)", data_name: "medium"***REMOVED***,
-  //       ***REMOVED***media: "(max-width: 640px)", data_name: "small"***REMOVED***,
-  //       ***REMOVED***media: "(max-width: 400px)", data_name: "x-small"***REMOVED***
-  //     ]
-  //   ***REMOVED***);
-
-    // Kinto client documentation at https://doc.esdoc.org/github.com/Kinto/kinto-client/#collections
-    // You can now access collections and records in Kinto like this
-    // var myCollection = kintoBucket.collection('YOUR_COLLECTION_NAME');
-    // myCollection.createRecord(***REMOVED***test:'value'***REMOVED***);
-    // myCollection.listRecords().then(function(data) ***REMOVED***console.log(data);***REMOVED***);
-
-    // Initialize your code here
-  // ***REMOVED***);
+  $(document).ready(function() ***REMOVED***
+    var adapter = window.dataAdapter || DataAdapter.getInstance();
+    adapter.getCoverage17(false, test);
+  ***REMOVED***);
 ***REMOVED***)();
