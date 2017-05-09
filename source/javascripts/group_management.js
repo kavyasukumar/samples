1677396ffc8c672ff209b3c3687063cdc0424a5c
Kavya Sukumar <kavya.sukumar@outlook.com>
2017-05-09 23:31:25 +0530

    small updates

--- source/javascripts/group_management.js
+++ source/javascripts/group_management.js
@@ -2,26 +2,32 @@
 //=require_tree ./_templates
 /* globals DataAdapter, console */
 
-/* If you would like to use Javascript templates, you can enable them by adding
- * the following line to the top of this file
- *   //=require_tree ./_templates
- * you can then use the templates like this:
- *   var template = window.JST['_templates/example'];
- *   var html = template(***REMOVED***my_var: 'Hello!', list: [1, 2, 3, 4]***REMOVED***);
- *   $('#example').html(html);
- * Template files should have the extension `.jst.ejs` to be properly loaded.
- */
-
 (function() ***REMOVED***
   var handleDataFetch = function (response) ***REMOVED***
-    console.log(response.length);
-    var template = window.JST['_templates/example'];
-    var html = template(***REMOVED***my_var: 'Hello!', list: [1, 2, 3, 4]***REMOVED***);
+    console.log(response);
+    debugger;
+    var byState = _.chain(response)
+	                  .groupBy(function(resp)***REMOVED*** return resp.state ***REMOVED***)
+                    .each(function(val, key, list)***REMOVED***
+		                    list[key] = _.groupBy(val, function(v)***REMOVED***
+                          return v.county_name;
+                        ***REMOVED***)
+                      ***REMOVED***)
+                    .sortBy(function(val, key)***REMOVED*** return key;***REMOVED***)
+                    .value();
+    var template = window.JST['_templates/provider_by_state'];
+    var html = template(***REMOVED***data: byState***REMOVED***);
     $('.body').html(html);
+    $(document).foundation('accordion', 'reflow');
   ***REMOVED***;
 
   $(document).ready(function() ***REMOVED***
     window.dataAdapter = window.dataAdapter || DataAdapter.getInstance();
     window.dataAdapter.getCoverage17(false, handleDataFetch);
+
+    window.dataAdapter.getProviderCount17(function(providerCount17)***REMOVED***
+      console.log('Here is the provider count for 2017 by fips code');
+      console.log(providerCount17);
+    ***REMOVED***);
   ***REMOVED***);
 ***REMOVED***)();
