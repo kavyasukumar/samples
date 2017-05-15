307b6a4f975e97ac1f6c0288a5ca8990fcefad97
caseymm <casey.miller@voxmedia.com>
2017-05-15 13:32:00 -0400

    re-render legend for now

--- source/javascripts/map_gen.js
+++ source/javascripts/map_gen.js
@@ -42,53 +42,57 @@
   //     .attr("height", height)
   //     .on("click", reset);
   //
-  // var g = svg.append("g").attr('class', 'counties');
-  // var x = d3.scaleLinear()
-  //     .domain([0, 4])
-  //     .rangeRound([600, 860]);
-  // var color = d3.scaleThreshold()
-  //     .domain(d3.range(0, 4))
-  //     .range(d3.schemeBlues[5]);
-  //
+  var x = d3.scaleLinear()
+      .domain([0, 4])
+      .rangeRound([600, 860]);
+  var color = d3.scaleThreshold()
+      .domain(d3.range(0, 4))
+      .range(d3.schemeBlues[5]);
+
   // svg
   //     .call(zoom) // delete this line to disable free zooming
   //     .call(zoom.transform, initialTransform);
+// .attr('transform', "translate(20,2.5) rotate(10)");
+  function drawScale()***REMOVED***
+    var g = svg.append("g").attr('class', 'scale')
+               .attr('transform', "translate(0,40)");
 
-  // g.selectAll("rect")
-  //   .data(color.range().map(function(d) ***REMOVED***
-  //       d = color.invertExtent(d);
-  //       if (d[0] == null) d[0] = x.domain()[0];
-  //       if (d[1] == null) d[1] = x.domain()[1];
-  //       return d;
-  //     ***REMOVED***))
-  //   .enter().append("rect")
-  //     .attr("height", 8)
-  //     .attr("x", function(d) ***REMOVED*** return x(d[0]) - 32.5; ***REMOVED***)
-  //     .attr("width", function(d) ***REMOVED*** return x(d[1]) - x(d[0]); ***REMOVED***)
-  //     .attr("fill", function(d) ***REMOVED*** return color(d[0]); ***REMOVED***);
-  //
-  // g.append("text")
-  //     .attr("class", "caption")
-  //     .attr("x", x.range()[0] - 32.5)
-  //     .attr("y", -6)
-  //     .attr("fill", "#000")
-  //     .attr("text-anchor", "start")
-  //     .attr("font-weight", "bold")
-  //     .text("Number of ACA insurers available");
-  //
-  // g.call(d3.axisBottom(x)
-  //     .tickFormat(function(x, i) ***REMOVED***
-  //       if(x === 3)***REMOVED***
-  //         return x + '+';
-  //       ***REMOVED*** else ***REMOVED***
-  //         return x;
-  //       ***REMOVED***
-  //     ***REMOVED***)
-  //     .tickPadding(13)
-  //     .tickSize(0)
-  //     .tickValues(color.domain()))
-  //   .select(".domain")
-  //     .remove();
+    g.selectAll("rect")
+      .data(color.range().map(function(d) ***REMOVED***
+          d = color.invertExtent(d);
+          if (d[0] == null) d[0] = x.domain()[0];
+          if (d[1] == null) d[1] = x.domain()[1];
+          return d;
+        ***REMOVED***))
+      .enter().append("rect")
+        .attr("height", 8)
+        .attr("x", function(d) ***REMOVED*** return x(d[0]) - 32.5; ***REMOVED***)
+        .attr("width", function(d) ***REMOVED*** return x(d[1]) - x(d[0]); ***REMOVED***)
+        .attr("fill", function(d) ***REMOVED*** return color(d[0]); ***REMOVED***);
+
+    g.append("text")
+        .attr("class", "caption")
+        .attr("x", x.range()[0] - 32.5)
+        .attr("y", -6)
+        .attr("fill", "#000")
+        .attr("text-anchor", "start")
+        .attr("font-weight", "bold")
+        .text("Number of ACA insurers available");
+
+    g.call(d3.axisBottom(x)
+        .tickFormat(function(x, i) ***REMOVED***
+          if(x === 3)***REMOVED***
+            return x + '+';
+          ***REMOVED*** else ***REMOVED***
+            return x;
+          ***REMOVED***
+        ***REMOVED***)
+        .tickPadding(13)
+        .tickSize(0)
+        .tickValues(color.domain()))
+      .select(".domain")
+        .remove();
+  ***REMOVED***
 
   function clicked() ***REMOVED***
     // if (active.node() === this) return reset();
@@ -150,13 +154,7 @@
         .attr("height", height)
         .attr("fill", 'none');
 
-    var g = svg.append("g").attr('class', 'counties');
-    var x = d3.scaleLinear()
-        .domain([0, 4])
-        .rangeRound([600, 860]);
-    var color = d3.scaleThreshold()
-        .domain(d3.range(0, 4))
-        .range(d3.schemeBlues[5]);
+    drawScale();
 
     svg
         .call(zoom) // delete this line to disable free zooming
