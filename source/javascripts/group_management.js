4fbe89cadf527af95ca7d75fba572bc20da5d69f
Kavya Sukumar <kavya.sukumar@outlook.com>
2017-05-17 18:13:42 +0530

    remove dummy and add update handlers

--- source/javascripts/group_management.js
+++ source/javascripts/group_management.js
@@ -3,13 +3,17 @@
 //= require _underscore_mixins
 
 /* globals DataAdapter, console */
-var dummy = [***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48317","is_active":true,"county_name":"Martin County","provider_id":33602,"provider_name":"Blue Cross and Blue Shield of Texas","id":"63402604-33c2-4758-8455-5d0a3872c373","last_modified":1494403630494***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48317","is_active":true,"county_name":"Martin County","provider_id":63509,"provider_name":"Allegian Health Plans","id":"6bc6ee01-a11e-4c2b-bc66-9d17cdfb6fdb","last_modified":1494403630447***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48317","is_active":true,"county_name":"Martin County","provider_id":91716,"provider_name":"Aetna","id":"2c8b5acb-778b-4193-b19d-bce080ea24ce","last_modified":1494403630419***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48315","is_active":true,"county_name":"Marion County","provider_id":37392,"provider_name":"Prominence Health Plan","id":"114915ba-ddc0-4fba-92e6-5145acee152f","last_modified":1494403630391***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48315","is_active":true,"county_name":"Marion County","provider_id":33602,"provider_name":"Blue Cross and Blue Shield of Texas","id":"72a80e5c-f4eb-4b92-bdfa-f0a7ddc2854d","last_modified":1494403630371***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48315","is_active":true,"county_name":"Marion County","provider_id":63509,"provider_name":"Allegian Health Plans","id":"2a228fc0-6195-4364-b89c-974382ab6d2b","last_modified":1494403630353***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48313","is_active":true,"county_name":"Madison County","provider_id":40788,"provider_name":"Scott and White Health Plan","id":"3dbce557-9d92-4cc1-9dd6-89497efd48b5","last_modified":1494403630318***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48313","is_active":true,"county_name":"Madison County","provider_id":37392,"provider_name":"Prominence Health Plan","id":"2dcb96dc-0379-49dd-9bb4-b25e27d943e2","last_modified":1494403630286***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48313","is_active":true,"county_name":"Madison County","provider_id":37755,"provider_name":"Insurance Company of Scott & White","id":"266a8ae2-61f2-4e23-8cab-1abba13f8ed5","last_modified":1494403630257***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48313","is_active":true,"county_name":"Madison County","provider_id":33602,"provider_name":"Blue Cross and Blue Shield of Texas","id":"e00b9131-3470-451e-9ece-a9de2584840f","last_modified":1494403630233***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48313","is_active":true,"county_name":"Madison County","provider_id":63509,"provider_name":"Allegian Health Plans","id":"27f05bcb-3591-452e-8593-be1a45f9bb23","last_modified":1494403630214***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48313","is_active":true,"county_name":"Madison County","provider_id":91716,"provider_name":"Aetna","id":"32d55cfb-0cca-425a-9f4b-99833cd87765","last_modified":1494403630190***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48305","is_active":true,"county_name":"Lynn County","provider_id":37392,"provider_name":"Prominence Health Plan","id":"cd087267-3c58-49bc-8c84-9d0f30758233","last_modified":1494403630169***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48305","is_active":true,"county_name":"Lynn County","provider_id":26539,"provider_name":"FirstCare Health Plans","id":"9e301a69-c533-4ded-acd0-99da26330bda","last_modified":1494403630149***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48305","is_active":true,"county_name":"Lynn County","provider_id":33602,"provider_name":"Blue Cross and Blue Shield of Texas","id":"f19d32d9-e46b-48a6-ae5b-74f18fb41f7e","last_modified":1494403630127***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48305","is_active":true,"county_name":"Lynn County","provider_id":63509,"provider_name":"Allegian Health Plans","id":"3b87e870-5783-41d5-8782-acf24df48648","last_modified":1494403630108***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48303","is_active":true,"county_name":"Lubbock County","provider_id":26539,"provider_name":"FirstCare Health Plans","id":"36aff90c-ba70-4a5d-b447-08a772435b1b","last_modified":1494403630083***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48303","is_active":true,"county_name":"Lubbock County","provider_id":33602,"provider_name":"Blue Cross and Blue Shield of Texas","id":"913b09c9-aa3d-4f62-bebe-194ae9d05811","last_modified":1494403630049***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48301","is_active":true,"county_name":"Loving County","provider_id":37392,"provider_name":"Prominence Health Plan","id":"ceb3498a-3a96-4ee3-8893-6d740f498e99","last_modified":1494403630016***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48301","is_active":true,"county_name":"Loving County","provider_id":26539,"provider_name":"FirstCare Health Plans","id":"bd6a2175-b27c-4312-af3a-6adb39b69d75","last_modified":1494403629983***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48211","is_active":true,"county_name":"Hemphill County","provider_id":63509,"provider_name":"Allegian Health Plans","id":"3fae099c-70ba-4594-8a19-fad545c81f3e","last_modified":1494403622122***REMOVED***];
 
 (function() ***REMOVED***
   var parameterize = function(input)***REMOVED***
     return input.toLowerCase()
                 .replace(/\[|\]|\(|\)|\***REMOVED***|\***REMOVED***|\\|\/|\.|\&/g, '')
                 .replace(/\s/g, '-');
+  ***REMOVED***;
+
+  var beforeSave = function()***REMOVED***
+    $('a.submit').text('saving...');
+    saveChanges();
   ***REMOVED***;
 
   var handleDataFetch = function (response) ***REMOVED***
@@ -31,6 +35,9 @@ var dummy = [***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48317
     var html = template(***REMOVED***data: byProvider, parameterize: parameterize***REMOVED***);
     $('.body').html(html);
     $(document).foundation('accordion', 'reflow');
+    $('.state-toggle').on('change', handleStateToggle);
+    $('.county-toggle').on('change', handleCountyToggle);
+    $('a.submit').on('click', beforeSave);
   ***REMOVED***;
 
   var handleStateToggle = function()***REMOVED***
@@ -51,12 +58,30 @@ var dummy = [***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48317
     $(stateToggle).prop('checked', checkedState);
   ***REMOVED***;
 
-  $(document).ready(function() ***REMOVED***
-    // window.dataAdapter = window.dataAdapter || DataAdapter.getInstance();
-    // window.dataAdapter.getCoverage(2017, false, handleDataFetch);
-    handleDataFetch(dummy);
-    $('.state-toggle').on('change', handleStateToggle);
-    $('.county-toggle').on('change', handleCountyToggle);
+  var afterSave = function()***REMOVED***
+    $('a.submit').text('saved');
+    console.log('aftersave');
+  ***REMOVED***;
 
+  var saveChanges = function()***REMOVED***
+    var changedRecords = [];
+    window.dataAdapter.getCoverage(2017, false, function(response)***REMOVED***
+      _.each(response, function(record)***REMOVED***
+        var id = '#' + record.id,
+            is_active = record.is_active,
+            is_checked =  $(id).prop('checked');
+
+        if (is_active !== is_checked)***REMOVED***
+          var newRecord = ***REMOVED***id : record.id, is_active : is_checked***REMOVED***;
+          changedRecords.push(newRecord);
+        ***REMOVED***
+      ***REMOVED***);
+      window.dataAdapter.updateCoverage(2017, changedRecords, afterSave);
+    ***REMOVED***);
+  ***REMOVED***;
+
+  $(document).ready(function() ***REMOVED***
+    window.dataAdapter = window.dataAdapter || DataAdapter.getInstance();
+    window.dataAdapter.getCoverage(2017, false, handleDataFetch);
   ***REMOVED***);
 ***REMOVED***)();
