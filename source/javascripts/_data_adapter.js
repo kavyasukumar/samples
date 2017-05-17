67dbf168f713d27a97e4a10e1624f8aca8988b22
Kavya Sukumar <kavya.sukumar@outlook.com>
2017-05-17 18:14:29 +0530

    add dummy and fix update fx

--- source/javascripts/_data_adapter.js
+++ source/javascripts/_data_adapter.js
@@ -3,7 +3,7 @@
 
 /* globals KINTO_TOKEN, console */
 
-
+ var dummy = [***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48317","is_active":true,"county_name":"Martin County","provider_id":33602,"provider_name":"Blue Cross and Blue Shield of Texas","id":"63402604-33c2-4758-8455-5d0a3872c373","last_modified":1494403630494***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48317","is_active":true,"county_name":"Martin County","provider_id":63509,"provider_name":"Allegian Health Plans","id":"6bc6ee01-a11e-4c2b-bc66-9d17cdfb6fdb","last_modified":1494403630447***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48317","is_active":true,"county_name":"Martin County","provider_id":91716,"provider_name":"Aetna","id":"2c8b5acb-778b-4193-b19d-bce080ea24ce","last_modified":1494403630419***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48315","is_active":true,"county_name":"Marion County","provider_id":37392,"provider_name":"Prominence Health Plan","id":"114915ba-ddc0-4fba-92e6-5145acee152f","last_modified":1494403630391***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48315","is_active":true,"county_name":"Marion County","provider_id":33602,"provider_name":"Blue Cross and Blue Shield of Texas","id":"72a80e5c-f4eb-4b92-bdfa-f0a7ddc2854d","last_modified":1494403630371***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48315","is_active":true,"county_name":"Marion County","provider_id":63509,"provider_name":"Allegian Health Plans","id":"2a228fc0-6195-4364-b89c-974382ab6d2b","last_modified":1494403630353***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48313","is_active":true,"county_name":"Madison County","provider_id":40788,"provider_name":"Scott and White Health Plan","id":"3dbce557-9d92-4cc1-9dd6-89497efd48b5","last_modified":1494403630318***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48313","is_active":true,"county_name":"Madison County","provider_id":37392,"provider_name":"Prominence Health Plan","id":"2dcb96dc-0379-49dd-9bb4-b25e27d943e2","last_modified":1494403630286***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48313","is_active":true,"county_name":"Madison County","provider_id":37755,"provider_name":"Insurance Company of Scott & White","id":"266a8ae2-61f2-4e23-8cab-1abba13f8ed5","last_modified":1494403630257***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48313","is_active":true,"county_name":"Madison County","provider_id":33602,"provider_name":"Blue Cross and Blue Shield of Texas","id":"e00b9131-3470-451e-9ece-a9de2584840f","last_modified":1494403630233***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48313","is_active":true,"county_name":"Madison County","provider_id":63509,"provider_name":"Allegian Health Plans","id":"27f05bcb-3591-452e-8593-be1a45f9bb23","last_modified":1494403630214***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48313","is_active":true,"county_name":"Madison County","provider_id":91716,"provider_name":"Aetna","id":"32d55cfb-0cca-425a-9f4b-99833cd87765","last_modified":1494403630190***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48305","is_active":true,"county_name":"Lynn County","provider_id":37392,"provider_name":"Prominence Health Plan","id":"cd087267-3c58-49bc-8c84-9d0f30758233","last_modified":1494403630169***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48305","is_active":true,"county_name":"Lynn County","provider_id":26539,"provider_name":"FirstCare Health Plans","id":"9e301a69-c533-4ded-acd0-99da26330bda","last_modified":1494403630149***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48305","is_active":true,"county_name":"Lynn County","provider_id":33602,"provider_name":"Blue Cross and Blue Shield of Texas","id":"f19d32d9-e46b-48a6-ae5b-74f18fb41f7e","last_modified":1494403630127***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48305","is_active":true,"county_name":"Lynn County","provider_id":63509,"provider_name":"Allegian Health Plans","id":"3b87e870-5783-41d5-8782-acf24df48648","last_modified":1494403630108***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48303","is_active":true,"county_name":"Lubbock County","provider_id":26539,"provider_name":"FirstCare Health Plans","id":"36aff90c-ba70-4a5d-b447-08a772435b1b","last_modified":1494403630083***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48303","is_active":true,"county_name":"Lubbock County","provider_id":33602,"provider_name":"Blue Cross and Blue Shield of Texas","id":"913b09c9-aa3d-4f62-bebe-194ae9d05811","last_modified":1494403630049***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48301","is_active":true,"county_name":"Loving County","provider_id":37392,"provider_name":"Prominence Health Plan","id":"ceb3498a-3a96-4ee3-8893-6d740f498e99","last_modified":1494403630016***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48301","is_active":true,"county_name":"Loving County","provider_id":26539,"provider_name":"FirstCare Health Plans","id":"bd6a2175-b27c-4312-af3a-6adb39b69d75","last_modified":1494403629983***REMOVED***,***REMOVED***"state":"TX","schema":1494402713398,"fips_code":"48211","is_active":true,"county_name":"Hemphill County","provider_id":63509,"provider_name":"Allegian Health Plans","id":"3fae099c-70ba-4594-8a19-fad545c81f3e","last_modified":1494403622122***REMOVED***];
 var DataAdapter = (function() ***REMOVED***
   var instance;
 
@@ -19,10 +19,13 @@ var DataAdapter = (function() ***REMOVED***
 
     //Public properties and methods
     _instance.getCoverage = function(year, refresh, callBackFunction) ***REMOVED***
+      // callBackFunction.call(this, dummy);
+      // return;
       var dataKey = 'coverage-' + year;
       if (!refresh && _data[dataKey]) ***REMOVED***
         console.log('data exists');
         callBackFunction.call(this, _data[dataKey]);
+        return;
       ***REMOVED***
 
       var collection = _kintoBucket.collection(dataKey);
@@ -41,27 +44,40 @@ var DataAdapter = (function() ***REMOVED***
     ***REMOVED***;
 
     _instance.updateCoverage = function(year, records, callBackFunction) ***REMOVED***
+      console.log('in updateCoverage');
       var dataKey = 'coverage-' + year,
         collection = _kintoBucket.collection(dataKey),
-        recordCount = records.count,
+        recordCount = records.length,
         processedCount = 0;
 
       _staleBit[dataKey] = true;
+      var i,j, chunk = 100, subset = [];
 
-      records.slice(100).each(function(subset) ***REMOVED***
-        collection.batch(function(batch) ***REMOVED***
-          for (var i = 0; i <= subset.length; i++) ***REMOVED***
-            batch.updateRecord(subset[i]);
-          ***REMOVED***
-        ***REMOVED***).then(function(response) ***REMOVED***
-          processedCount += 100;
-          if (processedCount >= recordCount) ***REMOVED***
-            callBackFunction.call(this);
-          ***REMOVED***
-        ***REMOVED***).catch(function(error) ***REMOVED***
-          console.log(error);
-        ***REMOVED***);
-      ***REMOVED***);
+      var batchUpdateFx = function(batch) ***REMOVED***
+        console.log('in batchUpdateFx');
+        for (var i = 0; i < subset.length; i++) ***REMOVED***
+          batch.updateRecord(subset[i], ***REMOVED*** patch: true ***REMOVED***);
+        ***REMOVED***
+      ***REMOVED***;
+
+      var batchHandleFx = function(response) ***REMOVED***
+        console.log('in batchHandleFx');
+        processedCount += chunk;
+        if (processedCount >= recordCount) ***REMOVED***
+        callBackFunction.call(this);
+        ***REMOVED***
+      ***REMOVED***;
+
+      var errorHandler = function(error) ***REMOVED***
+        console.log(error);
+      ***REMOVED***;
+
+      for (i=0,j=records.length; i<j; i+=chunk) ***REMOVED***
+        subset = records.slice(i,i+chunk);
+        collection.batch(batchUpdateFx)
+                  .then(batchHandleFx)
+                  .catch(errorHandler);
+      ***REMOVED***
     ***REMOVED***;
 
     _instance.getProviderCount = function(year, callBackFunction) ***REMOVED***
