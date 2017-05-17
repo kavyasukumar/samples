c2113743d53ce43659dc350edf7f5a4ca1d10e42
Kavya Sukumar <kavya.sukumar@outlook.com>
2017-05-17 18:12:39 +0530

    update shell script

--- process-data.sh
+++ process-data.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+export KINTO_API_TOKEN=***REMOVED***
 bundle exec rake shard_election_results
 bundle exec rake import_2017_coverage
 bundle exec rake import_providers
+bundle exec rake import_coverage_history
