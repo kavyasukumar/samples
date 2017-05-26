8acc84c993f8875c021fe8fe47f11b32d63490ae
Kavya Sukumar <kavya.sukumar@outlook.com>
2017-05-26 08:13:44 -0700

    linting

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -8,17 +8,6 @@ $(document).foundation();
 
 /* globals DataAdapter */
 
-
-/* If you would like to use Javascript templates, you can enable them by adding
- * the following line to the top of this file
- *   //=require_tree ./_templates
- * you can then use the templates like this:
- *   var template = window.JST['_templates/example'];
- *   var html = template(***REMOVED***my_var: 'Hello!', list: [1, 2, 3, 4]***REMOVED***);
- *   $('#example').html(html);
- * Template files should have the extension `.jst.ejs` to be properly loaded.
- */
-
 (function() ***REMOVED***
   window.commonErrorHandler = function(error) ***REMOVED***
     console.log(error);
